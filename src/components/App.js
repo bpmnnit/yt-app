08a2a2ca560fd18559575614ca5102b0341a6bb7
Bhanu Pratap Singh <bp.mnnit@gmail.com>
2021-07-07 11:52:41 +0530

    Latest news as default search term

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import useVideos from '../hooks/useVideos';
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const [videos, search] = useVideos('buildings');
+  const [videos, search] = useVideos('Latest News');
 
   useEffect(() => {
     setSelectedVideo(videos[0]);
